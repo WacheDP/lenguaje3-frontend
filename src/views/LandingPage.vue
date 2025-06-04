be813de95473fb19623a4acc1ccb26893133d770
yerson014 <201276989+yerson014@users.noreply.github.com>
2025-06-04 02:12:06 -0400

    Commit para la Pagina Don Cafe Belen 4/6/2025 2:12

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -20,6 +20,26 @@
             :product="product"
           />
         </div>
+        <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <br>
         <div style="text-align: center; margin-top: 2rem;">
              <button class="cta-button secondary" @click="verMasProductos">Ver Toda la Variedad</button>
         </div>
@@ -56,26 +76,6 @@
         <ContactForm @formSubmit="handleContactFormSubmit" />
       </div>
     </section>
-
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-          </a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
 
     <AppFooter />
   </div>
